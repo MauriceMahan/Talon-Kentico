4aa0615722264866e02f8a21e82864fb9501c403
Maurice Mahan <MauriceMahan@users.noreply.github.com>
2019-03-26 16:13:34 -0500

    Header/Main/Footer

--- src/js/globals/functions.js
+++ src/js/globals/functions.js
@@ -5,76 +5,13 @@
     /** FocusOverlay **/
     $("body").focusOverlay();
 
-    /** Image Gallery **/
-    $('.photo-gallery.rotating').slick({
-        dots: false,
-        speed: 300,
-        slidesToShow:4,
-        arrows: true,
-        slidesToScroll: 1,
-        responsive: [
-        {
-          breakpoint: 960,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-        {
-          breakpoint: 640,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
+    const hero = tns({
+        container: '.hero-banner',
+        mouseDrag: true,
+        autoplay: true
+    })
 
     $(".photo-overlay").colorbox({rel:'group1'});
-
-    $('.photo-gallery-thumbs').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.photo-thumbs'
-    });
-
-    $('.photo-thumbs').slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      asNavFor: '.photo-gallery-thumbs',
-      dots: false,
-      centerMode: true,
-      focusOnSelect: true,
-      arrows:true,
-
-      responsive: [
-        {
-          breakpoint: 960,
-          settings: {
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-
-        {
-          breakpoint: 479,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
 
     /** List Tool on load */
     $('.list-tool.expand-list, .list-tool.accordion-list').each(function(){
